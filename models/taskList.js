006dc3504fc20b15cb429f83ab468b20b3f9d555
Chemicalz82 <chemcope82@yahoo.com>
2018-09-27 14:53:03 -0500

    added definitions for data in schemas

--- models/taskList.js
+++ models/taskList.js
@@ -2,26 +2,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const taskListSchema = new Schema({
-    task_1_Description: ,
-    task_1_Location: ,
-    task_1_Location2: ,
-    task_1_PaymentAmount: ,
-    task_1_TimeDoneBy: ,
-    task_2_Description: ,
-    task_2_Location: ,
-    task_2_Location2: ,
-    task_2_PaymentAmount: ,
-    task_2_TimeDoneBy: ,
-    task_3_Description: ,
-    task_3_Location: ,
-    task_3_Location2: ,
-    task_3_PaymentAmount: ,
-    task_3_TimeDoneBy: ,
-    task_4_Description: ,
-    task_4_Location: ,
-    task_4_Location2: ,
-    task_4_PaymentAmount: ,
-    task_4_TimeDoneBy: 
+    user: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+        }
+    ],
+    task_1_Description: { type: String, required: true },
+    task_1_Location: { type: String, required: true },
+    task_1_Location2: { type: String },
+    task_1_PaymentAmount: { type: Number, required: true },
+    task_1_TimeDoneBy: { type: Date, required: true },
+    task_1_Done: { type: Boolean, default: false },
+    task_2_Description: { type: String, required: true },
+    task_2_Location: { type: String, required: true },
+    task_2_Location2: { type: String },
+    task_2_PaymentAmount: { type: Number, required: true },
+    task_2_TimeDoneBy: { type: Date, required: true },
+    task_2_Done: { type: Boolean, default: false },
+    task_3_Description: { type: String, required: true },
+    task_3_Location: { type: String, required: true },
+    task_3_Location2: { type: String },
+    task_3_PaymentAmount: { type: Number, required: true },
+    task_3_TimeDoneBy: { type: Date, required: true },
+    task_3_Done: { type: Boolean, default: false },
+    task_4_Description: { type: String, required: true },
+    task_4_Location: { type: String, required: true },
+    task_4_Location2: { type: String },
+    task_4_PaymentAmount: { type: Number, required: true },
+    task_4_TimeDoneBy: { type: Date, required: true },
+    task_4_Done: { type: Boolean, default: false },
 });
 
 const TaskList = mongoose.model("TaskList", taskListSchema);
--- models/user.js
+++ models/user.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstName: ,
-    lastName: ,
-    userName: ,
-    email: ,
-    password: ,
-    userImage: ,
-    phone: ,
-    creditCard: ,
-    ccExpire: ,
-    cardCcv: ,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    userName: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true, minlength: 8 }, 
+    userImage: { type: String, required: true },
+    phone: { type: String, required: true },
+    creditCard: { type: number, required: true },
+    ccExpire: { type: Date, required: true },
+    cardCcv: { type: Number, required: true }
 });
 
 const User = mongoose.model("User", userSchema);
