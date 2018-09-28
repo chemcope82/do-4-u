a6bf1e5d56acde311febf40216e60e2c22949cc7
TechMama1 <carla1thurston@gmail.com>
2018-09-28 11:17:29 -0500

    adding stripe to the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
+//STRIPE  step 2b
+
 import React, { Component } from 'react';
+import {Elements, StripeProvider} from 'react-stripe-elements';
+import CheckoutForm from './CheckoutForm';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      
-      </div>
+      <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+        <div className="example">
+          <h1>React Stripe Elements Example</h1>
+          <Elements>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </StripeProvider>
     );
   }
 }
