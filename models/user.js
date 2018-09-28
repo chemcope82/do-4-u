3c48252ab8f5ae2d9058a03c0e01fb8230420a17
Chemicalz82 <chemcope82@yahoo.com>
2018-09-28 14:32:43 -0500

    updated schemas

--- models/user.js
+++ models/user.js
@@ -2,14 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    userName: { type: String, required: true, unique: true },
-    email: { type: String, required: true },
+    firstName: { type: String, required: "First name is required" },
+    lastName: { type: String, required: "Last name is required" },
+    userName: { 
+        type: String, 
+        required: "Username is required", 
+        unique: true,
+        minlength: 8 
+    },
+    email: { 
+        type: String, 
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+        required: true 
+    },
     password: { type: String, required: true, minlength: 8 }, 
     userImage: { type: String, required: true },
-    phone: { type: String, required: true },
-    creditCard: { type: number, required: true },
+    //Request users to input phone numbers as 10-digit formatted as xxx-xxx-xxxx
+    phone: { 
+        type: String, 
+        required: true,
+        match: [/\d{3}-\d{3}-\d{4}/, "Please enter a 10-digit phone number of the format xxx-xxx-xxxx"] 
+    }, 
+    //Request users to input credit cards as 16-digits with no spaces or separators
+    creditCard: { 
+        type: String, 
+        required: true,
+        validate: [
+            function(input) {
+                return input.length === 16;
+            },
+            "Please enter a 16-digit credit card number with no spaces or separators."
+        ]
+    },
     ccExpire: { type: Date, required: true },
     cardCcv: { type: Number, required: true }
 });
