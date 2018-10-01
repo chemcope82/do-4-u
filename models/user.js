0c461c2dbe75b883424b536a63d6e22ff2eebe32
Chemicalz82 <chemcope82@yahoo.com>
2018-10-01 13:42:51 -0500

    implementing passport and jwt authorization

--- models/user.js
+++ models/user.js
@@ -1,15 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 const userSchema = new Schema({
     firstName: { type: String, required: "First name is required" },
     lastName: { type: String, required: "Last name is required" },
-    userName: { 
-        type: String, 
-        required: "Username is required", 
-        unique: true,
-        minlength: 8 
-    },
     email: { 
         type: String, 
         match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
@@ -37,6 +32,28 @@ const userSchema = new Schema({
     ccExpire: { type: Date, required: true },
     cardCcv: { type: Number, required: true }
 });
+
+userSchema.pre("save", function(next){
+    if(this.isModified("password") || this.isNew){
+        bcrypt.hash(this.password, null, null, (err, hash) => {
+            if(err){
+                console.log(err);
+                return next(err);
+            }
+            this.password = hash;
+            return(next);
+        });
+    }
+});
+
+userSchema.methods.comparePassword = function(pass, cb){
+    bcrypt.compare(pass, this.password, function(err, isMatch){
+        if(err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+}
 
 const User = mongoose.model("User", userSchema);
 
